7253d15e5491024db1b23165f505363efb0ffce3
王泽宇 <skyw95357@gmail.com>
2024-01-03 23:04:07 +0800

    add button signal

--- ControlSignal.py
+++ ControlSignal.py
@@ -1,6 +1,6 @@
 import asyncio
 from abc import ABC, abstractmethod
-
+import socket
 
 
 class ControlSignalBase:
@@ -48,3 +48,49 @@ class ControlSignalClock(ControlSignalBase):
             await asyncio.sleep(self._interval)
             self.stop()
             break
+        
+class ControlSignalWiFiButton(ControlSignalBase):
+    def __init__(self, ip, port):
+        super().__init__()
+        self.ip = ip
+        self.port = port
+        # self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.tcp_socket.connect((self.ip, self.port))
+    
+    async def connect_to_server(self):
+        self.tcp_reader, self.tcp_writer = await asyncio.open_connection(self.ip, self.port)
+        print(f"WiFi button connected! Address {self.ip}:{self.port}")
+        
+        
+    async def run(self):
+        self.tcp_reader, self.tcp_writer = await asyncio.open_connection(self.ip, self.port)
+        print(f"WiFi button connected! Address {self.ip}:{self.port}")
+        while True:
+            print("waiting for data")
+            # data = self.tcp_socket.recv(1024)
+            data = await self.tcp_reader.read(1024)
+            print(data)
+            if data:
+                print(f"received data: {data}")
+                if data == b'button pressed!':
+                    if not self.is_active():
+                        while not self.start():
+                            await asyncio.sleep(1)
+                        print("start signal")
+                    else:
+                        self.stop()
+                        print("stop signal")
+            else:
+                raise Exception("WiFi button connection broken")
+            await asyncio.sleep(1)
+                    
+                    
+if __name__ == '__main__':
+    async def test():
+        
+        # await control_signal.connect_to_server()
+        asyncio.create_task(control_signal.run())
+        # control_signal.run()
+    
+    control_signal = ControlSignalWiFiButton('192.168.31.125', 3328)
+    asyncio.run(control_signal.run())
